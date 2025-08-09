e225918f946f1cb00dacc023c46c03b5d1ae95f1
Deprime <a.dugaev@yandex.ru>
2025-08-09 13:23:08 +0500

    menu

---
+++ src/lib/components/structure/nav/Nav.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	const MENU = [
+		{
+			key: 'market',
+			label: 'Рынок',
+			href: '/market',
+			icon: 'market'
+		},
+		{
+			key: 'farm',
+			label: 'Ферма',
+			href: '/farm',
+			icon: 'farm'
+		},
+		{
+			key: 'home',
+			label: 'Капибыры',
+			href: '/app',
+			icon: 'home'
+		},
+		{
+			key: 'chest',
+			label: 'Сундук',
+			href: '/chest',
+			icon: 'chest'
+		},
+		{
+			key: 'friends',
+			label: 'Друзья',
+			href: '/friends',
+			icon: 'friends'
+		}
+	];
+
+	let menu = $derived.by(() => {
+		return MENU.map((item) => {
+			return {
+				...item,
+				active: page.url.pathname.startsWith(item.href)
+			};
+		});
+	});
+</script>
+
+<nav class="flex h-12 w-full justify-between">
+	<ul class="flex h-full w-full justify-between">
+		{#each menu as item}
+			<li class="flex h-full w-full flex-col items-center justify-center">
+				<a href={item.href} class:text-slate-500={!item.active}>
+					{item.label}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,6 +2,9 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 
+	// Components
+	import { Nav } from '$lib/components/structure';
+
 	// API
 	import { userApi } from '$lib/api/user';
 
@@ -34,17 +37,25 @@
 	});
 </script>
 
-<main class="mx-auto flex min-h-screen max-w-2xl flex-col items-center">
-	<header>
-		<p class="text-sm font-bold">
-			{$userStore.uuid}
-		</p>
+<main
+	class="order mx-auto flex min-h-screen max-w-2xl flex-col items-center border border-slate-200"
+>
+	<header class="flex w-full items-center justify-between border-b border-b-slate-200 p-2">
+		<div class="text-xs font-bold">
+			User UUID: {$userStore.nickname}
+		</div>
+		<div class="text-xs font-bold">
+			<button onclick={onLogout} class="cursor-pointer px-4 py-2 text-xs text-slate-500/70">
+				Logout
+			</button>
+		</div>
 	</header>
+
 	{@render children()}
 </main>
 
-<footer class="sticky bottom-0">
-	<button onclick={onLogout} class="cursor-pointer px-4 py-2 text-xs text-slate-500/50">
-		Logout
-	</button>
+<footer
+	class="sticky bottom-0 z-10 flex w-full justify-between border-t border-t-slate-200 bg-green-100"
+>
+	<Nav />
 </footer>
